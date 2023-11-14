da6e403b370f4db5cd5523cdad1265bfb0396cd7
Antox <froooglll78@gmail.com>
2023-11-14 17:48:13 +0300

    success

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
+function solveEquation(a,b,c){ 
   
-  return arr;
+  desk = Math.pow(b,2)+4*a*c;
+  if (desk === 0){
+    let sum = [];
+    sum=(-b)/(2*a);
+    return sum;
+  }
+  else if(desk > 0){
+    let sum = [];
+    sum = (-b + Math.sqrt(d) )/(2*a);
+    sum = (-b - Math.sqrt(d) )/(2*a);
+    return sum;  
+  }
+  else{
+    let desk=[];
+    return desk;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
