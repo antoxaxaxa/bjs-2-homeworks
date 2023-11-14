7c7670abbc3486dad6a1748251d30dd98e3f3ad0
Antox <froooglll78@gmail.com>
2023-11-14 17:51:01 +0300

    success

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,13 +4,15 @@ function solveEquation(a,b,c){
   desk = Math.pow(b,2)+4*a*c;
   if (desk === 0){
     let sum = [];
-    sum=(-b)/(2*a);
+    otv=(-b)/(2*a);
+    sum.push(otv);
     return sum;
   }
   else if(desk > 0){
     let sum = [];
-    sum = (-b + Math.sqrt(d) )/(2*a);
-    sum = (-b - Math.sqrt(d) )/(2*a);
+    otv1 = (-b + Math.sqrt(d) )/(2*a);
+    otv2 = (-b - Math.sqrt(d) )/(2*a);
+    sum.push(otv,otv2);
     return sum;  
   }
   else{
