7c6497738ddb77c33ae9f755f6ed10d7867249a0
Antox <froooglll78@gmail.com>
2023-11-18 17:17:50 +0300

    success

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,32 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(arr1, arr2){
+    if (arr1.length !== arr2.length){
+        return false;
+    }
+
+    const isTrue = arr1.every((element,i) =>{
+        return element === arr2[i];
+    })
+    return isTrue;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
   
-}
+  
+
+  const filteredUsers = users.filter((user) => user.gender === gender);
+  
+  if (filteredUsers.length === 0 || (gender !== "мужской" && gender !== "женский")) {
+    return 0; 
+  }
+
+  const ageSum = filteredUsers.map((user) => user.age ).reduce((a, b) => a + b);
+  const result = ageSum/filteredUsers.length;
+  
+  
+ 
+ 
+  return result;
+}  
+
+
+console.log(getUsersNamesInAgeRange([1,2],"мужской"));
